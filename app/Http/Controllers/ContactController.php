4c40a49af6644f1cda526efee0df308dbb3ee1f1
Joy Faith Prieto <joypitprieto@gmail.com>
2026-02-11 16:28:48 +0800

    add try catch mail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,12 +18,19 @@ class ContactController extends Controller
     // Authenticated user is automatically the default API user
     //$user = $request->user();
     $userEmail = $request->user() ? $request->user()->email : 'guest';
-    Mail::to(env('CONTACT_RECEIVER_EMAIL'))
-    ->send(new ContactMessageMail(
-        $validated['name'],
-        $validated['email'],
-        $validated['message']
-    ));
+    try {
+        Mail::to(env('CONTACT_RECEIVER_EMAIL'))
+            ->send(new ContactMessageMail(
+                $validated['name'],
+                $validated['email'],
+                $validated['message']
+            ));
+    } catch (\Exception $e) {
+        return response()->json([
+            'error' => 'Mail sending failed',
+            'message' => $e->getMessage()
+        ], 500);
+    }
 
     return response()->json([
         'message' => 'Your message has been sent successfully!',
