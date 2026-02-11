417e068f22ff7c5927f1c905a1de6df58147ecc2
Joy Faith Prieto <joypitprieto@gmail.com>
2026-02-11 15:57:27 +0800

    test user

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -17,25 +17,28 @@ class ContactController extends Controller
 
     // Authenticated user is automatically the default API user
     //$user = $request->user();
-
-     Mail::to(env('CONTACT_RECEIVER_EMAIL', 'joypitprieto@gmail.com'))
-        ->send(new ContactMessageMail(
-            $validated['name'],
-            $validated['email'],
-            $validated['message']
-        ));
-
+    $userEmail = $request->user() ? $request->user()->email : 'guest';
+    //  Mail::to(env('CONTACT_RECEIVER_EMAIL', 'joypitprieto@gmail.com'))
+    //     ->send(new ContactMessageMail(
+    //         $validated['name'],
+    //         $validated['email'],
+    //         $validated['message']
+    //     ));
     return response()->json([
         'message' => 'Your message has been sent successfully!',
-        'submitted_by' => $request->email,
+        'submitted_by' => $userEmail,
     ], 200);
+    // return response()->json([
+    //     'message' => 'Your message has been sent successfully!',
+    //     'submitted_by' => $request->email,
+    // ], 200);
     
     // You can save the contact message to DB, send email, etc.
 
-    return response()->json([
-        'message' => 'Contact received successfully',
-        'submitted_by' => 'api@system.local',
-    ]);
+    // return response()->json([
+    //     'message' => 'Contact received successfully',
+    //     'submitted_by' => 'api@system.local',
+    // ]);
 }
 
 }
