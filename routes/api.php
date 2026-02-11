3e3425631568755fae14f8398a7a9fe1bdb45bac
Joy Faith Prieto <joypitprieto@gmail.com>
2026-02-11 15:28:07 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AuthController;
 
+// Handle all OPTIONS preflight requests
+Route::options('{any}', function () {
+    return response()->json([], 200);
+})->where('any', '.*');
+
 Route::post('/login', [AuthController::class, 'login']); // optional
 Route::post('/contact', [ContactController::class, 'store']);
